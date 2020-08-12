cda97c5e31d8843bef9cd22db9f263b71d41417e
refixshow <adasscieszka@wp.pl>
2020-08-12 19:29:13 +0200

    better solution condition[1] fail

--- Switch-old.js
+++ Switch-old.js
@@ -1,19 +1,36 @@
 // Stwórz klasę Switch, która służy do wielokrotnej, równorzędnej walidacji
 // - ma metodę .add w której dodajemy warunek do sprawdzenia oraz callback, który ma się wywołać jak zostanie warunek spełniony
-// - ma metodę .isValid, która iteruje po wszystkich .cases sprawdzając każdy dodany wcześniej warunek
+// - ma metodę .isValid, która iteruje po wszystkich .cases sprawdzając kążdy dodany wcześniej warunek
 // - metoda .isValid, która zwraca true jeśli wszystkie warunki będą na false
 
 // po wykonaniu w metodzie .isValid dany warunek jest usuwany z listy cases
 
 class Switch {
-  private cases: Array<boolean> = [];
-  private conditions: Array<boolean> = [];
+  cases = [];
+  conditions = [];
 
   add(condition, callback) {
-    
+    this.cases.push([condition, callback]);
   }
+
   isValid() {
-    // return this.conditions
+    this.cases.forEach((Case) => {
+      if (Case[0]) {
+        Case[1]();
+        this.conditions.push(false);
+      } else {
+        this.conditions.push(true);
+      }
+    });
+    this.cases = [];
+
+    const temp = this.conditions.filter(el === true);
+    this.conditions = [];
+    if (temp.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
---
+++ Switch.ts
@@ -0,0 +1,21 @@
+class DynamicSwitch {
+  private conditions: (boolean | (() => void))[][] = [];
+
+  add(condition: boolean, callback: () => void): void {
+    this.conditions.push([condition, callback]);
+  }
+
+  isValid(): boolean {
+    let areConditionsValid = true;
+    this.conditions.forEach((condition) => {
+      if (condition[0]) {
+        condition[1]();
+        areConditionsValid = false;
+      }
+    });
+    this.conditions = [];
+    return areConditionsValid;
+  }
+}
+
+module.exports = DynamicSwitch;
